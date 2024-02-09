b175eddf3c73893d913f82ba174661fa04c511d6
=) <valtteri.ikonen@metropolia.fi>
2024-02-09 12:37:46 +0200

    drivercard & css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// App.js
-import React from 'react';
 import Rating from './components/Rating';
+import Drivercard from './components/Drivercard';
 import './App.css';
 
 function App() {
   return (
     <div className="Iterations">
       <div className="Ratings">
+        <h1>Iteration 1: Ratings</h1>
         <Rating stars={0} />
         <Rating stars={1} />
         <Rating stars={2} />
@@ -14,6 +14,10 @@ function App() {
         <Rating stars={4} />
         <Rating stars={5} />
       </div>
+      <>
+      <h1>Iteration 2: Driver Cards</h1>
+        <Drivercard /> 
+      </>
     </div>
   );
 }
